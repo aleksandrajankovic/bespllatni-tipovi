34b7e441b8058a963d7253f337353443f99a6a72
iamalexja <aleksandra.bgd.87@gmail.com>
2024-05-27 13:59:02 +0200

    add new api

--- tipovi/src/redux/api.js
+++ tipovi/src/redux/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "https://besplatni-tipovi-api.vercel.app/",
+});
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     req.headers.Authorization = `Bearer ${
