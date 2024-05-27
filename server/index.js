ae994202aa0035532fc957804f71da063d451f3f
iamalexja <aleksandra.bgd.87@gmail.com>
2024-05-27 12:33:36 +0200

    env added

--- server/index.js
+++ server/index.js
@@ -4,27 +4,28 @@ import cors from "cors";
 import morgan from "morgan";
 import userRouter from "./routes/user.js";
 import tipsRouter from "./routes/tips.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express();
 
 app.use(morgan("dev"));
-app.use(express.json({ limit: "30mb", extended: true })); //parsiranje dolazecih json podataka
+app.use(express.json({ limit: "30mb", extended: true })); // parsiranje dolazecih json podataka
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-//setup ruta
+// Setup ruta
 app.use("/users", userRouter); // http://localhost:5000/users/signup
 app.use("/tip", tipsRouter);
 
-
-const MONGODB_URL = "mongodb://0.0.0.0:27017";
-const port = 5000;
+const MONGODB_URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(MONGODB_URL)
+  .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Baza podataka je uspešno povezana!");
-    app.listen(port, () => console.log(`Server running on port ${port}`));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((error) => console.log(`${error} did not connect`));
