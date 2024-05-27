6c29f12679627323eced47ae0f9443a139c7bb20
iamalexja <aleksandra.bgd.87@gmail.com>
2024-05-27 17:23:29 +0200

    config index.js

--- server/index.js
+++ server/index.js
@@ -13,6 +13,14 @@ app.use(morgan("dev"));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
+// Dodavanje CORS zaglavlja ručno
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/users", userRouter);
 app.use("/tip", tipsRouter);
 
