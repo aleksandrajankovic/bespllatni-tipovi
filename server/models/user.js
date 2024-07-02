f868cdef352f38aa69d6bf46e3c526003e524cb3
aleksandrajankovic <aleksandra.bgd.87@gmail.com>
2024-07-02 16:31:38 +0200

    Mongoose TTL

--- server/models/user.js
+++ server/models/user.js
@@ -1,23 +1,38 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: false },
-  id: { type: String },
-  role: { type: String, default: "user" },
-  status: {
-    type: String,
-    enum: ["unverified", "verified"],
-    default: "unverified",
-    
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: false },
+    id: { type: String },
+    role: { type: String, default: "user" },
+    status: {
+      type: String,
+      enum: ["unverified", "verified"],
+      default: "unverified",
+    },
+    verificationToken: {
+      type: String,
+      default: null,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    expireAt: {
+      type: Date,
+      default: () => new Date(+new Date() + 2 * 60 * 1000), // Ističe za 2 minuta
+    },
   },
-  verificationToken: {
-    type: String,
-    default: null,
-  },
-  
-},
-{ timestamps: true } );
+  { timestamps: true }
+);
+
+userSchema.index({ expireAt: 1 }, { expireAfterSeconds: 0 });
 
 export default mongoose.model("User", userSchema);
+
